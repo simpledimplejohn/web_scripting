2e1a53e5bb29226b3fc76784b2ea60f32872d081
John Blalock <simpledimplejohn@gmail.com>
2023-08-10 21:04:51 -0400

    fixed script for single item

--- Splunk/all_configurations_ownership copy.js
+++ Splunk/all_configurations_ownership copy.js
@@ -1,7 +1,12 @@
 // this script works from inside the all configurations page once logged in 
-//  http://localhost:8000/en-US/account/login
 // http://localhost:8000/en-US/manager/launcher/bulkreassign?app=John&owner=&count=100&offset=0&search=&appOnly=true&sortDir=&sortKey=&orphaned=false&configType=savedsearch
+// this works for a single item
 
+
+const name = 'Customer alert'
+let name_link = `[aria-label="/servicesNS/nobody/John/saved/searches/${encodeURIComponent(name)}"]`
+console.log(name_link)
+let name_link2 = "#control-view7060"
 async function performActions() {
     const waitFor = (selector) => new Promise(resolve => {
       const interval = setInterval(() => {
@@ -17,12 +22,8 @@ async function performActions() {
       const element = await waitFor(selector);
       element.click();
     };
-  
-    await clickAndWait('#control-view7097');
-    await clickAndWait('#control-view7093');
-    await clickAndWait('#control-view7088');
-  
+   
+    await clickAndWait(name_link); 
     console.log('Actions completed.');
   }
-  
   performActions();
